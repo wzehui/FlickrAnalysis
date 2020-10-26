bc0f799e48e3e5564d57f57e0dbb2db3716f5265
wzehui <wzehui@hotmail.com>
2020-10-26 21:27:59 +0100

    test from Mac

--- Analyse.py
+++ Analyse.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 from datetime import datetime
 
 # load Flickr data from CSV to DataFrame
@@ -35,4 +36,28 @@ time_interval = pd.DataFrame(columns=['owner','date_min','date_max'])
 #                     time_interval.loc[time_interval['owner'].str.contains(df.owner[i]), 'date_min'] = df.datetaken[i]
 #                 elif datetime.strptime(df.datetaken[i],format) > datetime.strptime(item.loc[idx].iat[2],format):
 #                     time_interval.loc[time_interval['owner'].str.contains(df.owner[i]), 'date_max'] = df.datetaken[i]
-#
+
+owner = df['owner']
+owner_dict = {'owner': owner}
+owner = pd.DataFrame(owner_dict)
+owner.drop_duplicates('owner', keep="first", inplace=True, ignore_index=True)
+
+for owner_id in owner.itertuples():
+    item = df[df['owner'].str.contains(owner_id.owner)]
+    var_lati = (item.latitude*1e5).var()
+    var_long = (item.longitude*1e5).var()
+    if math.isnan(var_lati):
+        var_loc = 0
+    else:
+        var_loc = math.sqrt((var_lati + var_long)/(2*1e5))
+    idx = owner[owner['owner'].str.contains(owner_id.owner)].index
+    owner.loc[idx,'var_loc'] = var_loc
+
+# 30 days and holiday condition
+
+# location condition
+owner_d = owner.copy(deep=True)
+for item in owner.itertuples():
+    if item.var_loc > 20:
+        owner_d.drop(index=item.Index,inplace=True)
+
